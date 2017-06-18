e40b36c233563886b786f3f83bd02e25c7775882
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-18 22:54:43 +0530

    Initial commit

---
+++ app/src/main/java/com/appbusters/robinkamboj/facebook/MainActivity.java
@@ -0,0 +1,16 @@
+package com.appbusters.robinkamboj.facebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(this);
+    }
+}
