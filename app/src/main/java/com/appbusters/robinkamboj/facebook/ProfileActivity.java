ab6a3e9ac4523a76bd0fc18e9371643bd7a6f4a1
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-19 16:16:25 +0530

    show and hide implemented

--- app/src/main/java/com/appbusters/robinkamboj/facebook/ProfileActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/facebook/ProfileActivity.java
@@ -2,12 +2,14 @@ package com.appbusters.robinkamboj.facebook;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -24,6 +26,7 @@ public class ProfileActivity extends AppCompatActivity{
 
     private CardView hidenshowCardMain, hidenshowCardUnwanted;
     private LinearLayout hidenshowMain, hidenshowUnwanted;
+    private RadioGroup rgUserType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class ProfileActivity extends AppCompatActivity{
         hidenshowCardUnwanted = (CardView) findViewById(R.id.hidenshowcardunwanted);
         hidenshowMain = (LinearLayout) findViewById(R.id.hidenshowmain);
         hidenshowUnwanted = (LinearLayout) findViewById(R.id.hidenshowunwanted);
+        rgUserType = (RadioGroup) findViewById(R.id.rgusertype);
 
         Bundle params = new Bundle();
         params.putString("fields", "id,email,name,gender,cover,picture.type(large)");
@@ -69,19 +73,39 @@ public class ProfileActivity extends AppCompatActivity{
                         }
                     }
                 }).executeAsync();
+
+        rgUserType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int checkedId) {
+                switch (checkedId){
+                    case R.id.standard:{
+                        hideGuruStuff();
+                        break;
+                    }
+                    case R.id.guru:{
+                        showGuruStuff();
+                        break;
+                    }
+                }
+            }
+        });
     }
 
     public void showGuruStuff(){
-        hidenshowCardMain.setVisibility(View.VISIBLE);
-        hidenshowUnwanted.setVisibility(View.VISIBLE);
-        hidenshowMain.setVisibility(View.VISIBLE);
-        hidenshowCardUnwanted.setVisibility(View.VISIBLE);
+        if(hidenshowCardMain.getVisibility()==View.GONE){
+            hidenshowCardMain.setVisibility(View.VISIBLE);
+            hidenshowUnwanted.setVisibility(View.VISIBLE);
+            hidenshowMain.setVisibility(View.VISIBLE);
+            hidenshowCardUnwanted.setVisibility(View.VISIBLE);
+        }
     }
 
     public void hideGuruStuff(){
-        hidenshowCardMain.setVisibility(View.INVISIBLE);
-        hidenshowUnwanted.setVisibility(View.INVISIBLE);
-        hidenshowMain.setVisibility(View.INVISIBLE);
-        hidenshowCardUnwanted.setVisibility(View.INVISIBLE);
+        if(hidenshowCardMain.getVisibility()==View.VISIBLE){
+            hidenshowCardMain.setVisibility(View.GONE);
+            hidenshowUnwanted.setVisibility(View.GONE);
+            hidenshowMain.setVisibility(View.GONE);
+            hidenshowCardUnwanted.setVisibility(View.GONE);
+        }
     }
 }
