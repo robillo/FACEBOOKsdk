4ab41aee5c0d7438fe097999c44b9941ab553a17
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-19 16:04:29 +0530

    layout ids and references

--- app/src/main/java/com/appbusters/robinkamboj/facebook/ProfileActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/facebook/ProfileActivity.java
@@ -4,7 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -19,10 +22,18 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity{
 
+    private CardView hidenshowCardMain, hidenshowCardUnwanted;
+    private LinearLayout hidenshowMain, hidenshowUnwanted;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        hidenshowCardMain = (CardView) findViewById(R.id.hidenshowcardmain);
+        hidenshowCardUnwanted = (CardView) findViewById(R.id.hidenshowcardunwanted);
+        hidenshowMain = (LinearLayout) findViewById(R.id.hidenshowmain);
+        hidenshowUnwanted = (LinearLayout) findViewById(R.id.hidenshowunwanted);
 
         Bundle params = new Bundle();
         params.putString("fields", "id,email,name,gender,cover,picture.type(large)");
@@ -58,5 +69,19 @@ public class ProfileActivity extends AppCompatActivity{
                         }
                     }
                 }).executeAsync();
+    }
+
+    public void showGuruStuff(){
+        hidenshowCardMain.setVisibility(View.VISIBLE);
+        hidenshowUnwanted.setVisibility(View.VISIBLE);
+        hidenshowMain.setVisibility(View.VISIBLE);
+        hidenshowCardUnwanted.setVisibility(View.VISIBLE);
+    }
+
+    public void hideGuruStuff(){
+        hidenshowCardMain.setVisibility(View.INVISIBLE);
+        hidenshowUnwanted.setVisibility(View.INVISIBLE);
+        hidenshowMain.setVisibility(View.INVISIBLE);
+        hidenshowCardUnwanted.setVisibility(View.INVISIBLE);
     }
 }
